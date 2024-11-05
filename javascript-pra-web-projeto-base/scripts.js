101054a4570e7b920d93fe32873583d5a3de01e4
Isaaczmed <zanibonisaac@gmail.com>
2024-11-05 17:48:00 -0300

    Atualizando código

--- javascript-pra-web-projeto-base/scripts.js
+++ javascript-pra-web-projeto-base/scripts.js
@@ -1,7 +1,7 @@
 // Seleciona o input em que o item foi colocado pelo seu id
 const item = document.getElementById("input-item");
 // Seleciona o botão que salva o item na lista
-const botaoSalvarItem = document.getElementById("adicionar-botao");
+const botaoSalvarItem = document.getElementById("adicionar-item");
 // Seleciona o ul do carrinho de compras
 const listaDeCompras = document.getElementById("lista-de-compras");
 // Seleciona o ul da lista de comprados
@@ -23,7 +23,7 @@ function adicionarItem(evento) {
     // Adiciona na const containerItemDaLista o comando para criar um elemento ("div")
     const containerItemLista = document.createElement("div");
     // Adiciona uma class na div criada anteriormente
-    containerItemLista.classList.add("item-lista-container");
+    containerItemLista.classList.add("lista-item-container");
 
     // Adiciona na const containerNomeDoItem o comando para criar um elemento ("div")
     const containerNomeDoItem = document.createElement("div");
@@ -31,14 +31,14 @@ function adicionarItem(evento) {
     // Adiciona na const containerCheckbox o comando para criar um elemento ("div")
     const containerCheckbox = document.createElement("div");
     // Adiciona uma class na div criada anteriormente
-    containerCheckbox.classList.add("checkbox-container");
+    containerCheckbox.classList.add("container-checkbox");
 
     // Adiciona na const checkboxInput o comando para criar um elemento ("input")
     const checkboxInput = document.createElement("input");
     // Altera o tipo do input mudando o .type para "checkbox"
     checkboxInput.type = "checkbox";
     // Adiciona uma class no input criado anteriormente
-    checkboxInput.classList.add("checkbox-input");
+    checkboxInput.classList.add("input-checkbox");
     // Altera o .id do checkbox mudando pelo contador, aumentando para cada item adicionado
     checkboxInput.id = "checkbox-" + contador++;
 
@@ -53,20 +53,24 @@ function adicionarItem(evento) {
     checkboxLabel.addEventListener("click", function (evento) {
         /* Seleciona o evento e o elemento que está sendo clicado através do currentTarget.
         Com o querySelector() pode-se selecionar o conteúdo do elemento clicado que possuí 
-        a classe .checkbox-input */ 
-        const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
+        a classe .input-checkbox */ 
+        const checkboxInput = evento.currentTarget.querySelector(".input-checkbox");
         // Faz o mesmo para o checkbox customizado
         const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
-        // Seleciona o título do item mais perto 
+        // Obtém o elemento com o id "item-titulo" dentro do <li> mais próximo do elemento que disparou o evento ("click")
         const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
 
         // Verifica caso o input está checked, caso esteja, adiciona a class "checked" e na lista de comprados,
         // caso não retira dos dois
         if (checkboxInput.checked) {
             checkboxCustomizado.classList.add("checked");
+            // Muda a decoração do .style do itemTitulo para line-through
+            itemTitulo.style.textDecoration = "line-through";
             listaComprados.appendChild(itemDaLista);
         } else {
             checkboxCustomizado.classList.remove("checked");
+            // Retira a decoração do .style do itemTitulo
+            itemTitulo.style.textDecoration = "none";
             listaDeCompras.appendChild(itemDaLista);
         }
     });
@@ -100,11 +104,11 @@ function adicionarItem(evento) {
     // Adiciona na const botaoRemover o comando para criar um elemento ("button")
     const botaoRemover = document.createElement("button");
     // Adiciona uma class no botão criado anteriormente
-    botaoRemover.classList.add("botao-acao");
+    botaoRemover.classList.add("item-lista-button");
     // Adiciona na const botaoEditar o comando para criar um elemento ("button")
     const botaoEditar = document.createElement("button");
     // Adiciona uma class no botão criado anteriormente
-    botaoEditar.classList.add("botao-acao");
+    botaoEditar.classList.add("item-lista-button");
 
     // Adiciona na const imagemRemover o comando para criar um elemento ("img")
     const imagemRemover = document.createElement("img");
@@ -134,8 +138,17 @@ function adicionarItem(evento) {
     // Faz com que o containerBotoes("div") fique identado dentro do containerItemLista("div") 
     containerItemLista.appendChild(containerBotoes);
 
+    // Cria um elemento ("p") na const itemData
+    const itemData = document.createElement("p");
+    // Altera o innerText do itemData, colocando a data do dia da semana, a data em números e o horário
+    itemData.innerText = `${new Date().toLocaleDateString("pt-BR", { weekday: "long"})} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"})}`;
+    // Adiciona na class do itemData a class "texto-data"
+    itemData.classList.add("texto-data");
+
     // Faz com que o containerItemLista("div") fique identado dentro do itemDaLista("li") 
     itemDaLista.appendChild(containerItemLista);
+    // Faz com que o itemData("p") fique identado dentro do containerItemLista("li") 
+    itemDaLista.appendChild(itemData);
     // Faz com que o itemDaLista("div") fique identado dentro da listaDeCompras("ul") 
     listaDeCompras.appendChild(itemDaLista);
 }
